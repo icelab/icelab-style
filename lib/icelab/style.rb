956700067e72801dba83f7ef157fe3cb1c625c46
Dylan Wolff <dylan@dylanwolff.com>
2017-07-28 17:19:30 +1000

    Initial commit

---
+++ lib/icelab/style.rb
@@ -0,0 +1,7 @@
+require "icelab/style/version"
+
+module Icelab
+  module Style
+    # Your code goes here...
+  end
+end
---
+++ lib/icelab/style/version.rb
@@ -0,0 +1,5 @@
+module Icelab
+  module Style
+    VERSION = "0.1.0"
+  end
+end
